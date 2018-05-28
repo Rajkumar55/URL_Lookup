eb27500a04931f620ade2d1e743d711033269158
Rajkumar <rajshine55@gmail.com>
2018-05-28 10:28:44 +0530

    functional test cases added

---
+++ url_lookup/tests/test_controllers.py
@@ -0,0 +1,17 @@
+from django.test import TestCase
+import json
+from rest_framework import status
+
+from url_lookup.controllers import URLInfo
+from url_lookup.models import URLBlacklist
+
+
+class URLInfoTestCase(TestCase):
+    model = URLBlacklist
+    fixtures = ['URLBlacklist.json']
+
+    def test_get_info(self):
+        is_url_exists = URLInfo.get_info(URLInfo(), 'example.com')
+        self.assertTrue(is_url_exists)
+        is_exists = URLInfo.get_info(URLInfo(), 'google.com')
+        self.assertFalse(is_exists)
